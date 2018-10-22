612f899a44907598fc9b0f4745adafe8a0dc732c
Rok Zidarn <rz3817@student.uni-lj.si>
2018-10-22 18:25:09 +0200

    Ready for word embeddings

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-TextSummaryKerasMaster
+TEXT SUMMARIZATION
+------------------
+Python + BS4 + NTLK + Tensorflow + Keras
+
+# word embeddings
+
+    word embeddings are a type of word representation that allows words with similar meaning to have a similar representation
+    words that have similar context will have similar meanings
+    individual words are represented as real-valued vectors in a predefined vector space
+    dense and low-dimensional vectors -> faster computation with neural networks
+    
+    algorithm:
+        word ('hello') -> one-hot encoded (0,0,0,1,0,0,...) -> reavl-valued vector (0.312, -0.110, -0.499, 0.765, ...)
+        word2vec: CBOW or skipgram, statistical method, CBOW -> predicts context based on current word
+        glove: matrix factorization technique (LSA), context defined by word co-occurance matrix
+
+    Keras:
+        Embedding(
+            input_dim='size_of_vocabulary', 
+            output_dim='size_of_desired_vector_space', 
+            input_length='number_of_words_in_document'
+        ) 
+        => returns learned weights, 2D vector with one embedding for each word in the input sequence of words,
+            to connect to a Dense layer, you must add Flatten() after Embedding()
+            
+        model = Sequential()
+        model.add(Embedding(1000, 64, input_length=10))  # 1000 possible values, 64D embedding       
+        input_array = np.random.randint(1000, size=(32, 10))  # values [0...999], 32x10   
+        model.compile('rmsprop', 'mse')
+        output_array = model.predict(input_array)  # 32x10x64
--- scraper.py
+++ scraper.py
@@ -3,7 +3,74 @@ import bs4
 import nltk
 import re
 
-"""URLS AND DATA SCRAPING"""
+def prepare_data(url):
+    html = requests.get(url)
+    data = bs4.BeautifulSoup(html.text, "lxml")
+
+    title_tag = data.find(name='h1', id='firstHeading', class_='firstHeading')
+    title = title_tag.text
+    #print(title)
+
+    all_text = data.find(name='div', class_='mw-parser-output')
+
+    first_child = None
+    for i in all_text.children:
+        first_child = i
+        break
+
+    if first_child.name != 'p':
+        tag = first_child.findNextSibling(name='p')
+    else:
+        tag = all_text.find('p')
+
+    summary = [tag.text]
+
+    while True:  # next paragraphs of summary until div tag
+        tag = tag.nextSibling
+        if isinstance(tag, bs4.element.Tag):
+            if tag.name == 'h2':
+                after = tag
+                break
+            elif tag.name == 'p':
+                summary.append(tag.text)
+
+    tags = after.findNextSiblings(name='p')  # all paragraphs of text
+    text = []
+    for t in tags:
+        text.append(t.text)
+
+    # print(summary)
+    # print(text)
+
+    summary_combined = ''
+    for paragraph in summary:
+        summary_combined = summary_combined + paragraph
+
+    text_combined = ''
+    for paragraph in text:
+        text_combined = text_combined + paragraph
+
+    summary_cleaned = re.sub('\[[0-9]*\]', '', summary_combined)
+    text_cleaned = re.sub('\[[0-9]*\]', '', text_combined)
+
+    # print(summary_combined)
+    # print(summary_cleaned)
+    # print(text_combined)
+    # print(text_cleaned)
+
+    summary_tokens = nltk.word_tokenize(summary_cleaned)
+    text_tokens = nltk.word_tokenize(text_cleaned)
+
+    exclude_list = [',', '.', '(', ')', '»', '«']
+    processed_summary_tokens = [e.lower() for e in summary_tokens if e not in exclude_list]
+    processed_text_tokens = [e.lower() for e in text_tokens if e not in exclude_list]
+
+    #print(processed_summary_tokens)
+    #print(processed_text_tokens)
+
+    return (title, processed_summary_tokens, processed_text_tokens)
+
+# MAIN
 
 data = [
     'https://sl.wikipedia.org/wiki/Domači_pes',
@@ -13,77 +80,19 @@ data = [
     'https://sl.wikipedia.org/wiki/Kristalna_noč'
 ]
 
-html = requests.get(data[0])
-data = bs4.BeautifulSoup(html.text, "lxml")
+titles = []
+summaries = []
+texts = []
+all_tokens = []
 
-title = data.find(name='h1', id='firstHeading', class_='firstHeading')
-print(title.text)
+for url in data:
+    (title, summary, text) = prepare_data(url)
+    print(title, '\n', summary, '\n', text, '\n')
 
-all_text = data.find(name='div', class_='mw-parser-output')
-
-first_child = None
-for i in all_text.children:
-    first_child = i
-    break
-
-tag = None  # first paragraph of summary
-
-if first_child.name != 'p':
-    tag = first_child.findNextSibling(name='p')
-else:
-    tag = all_text.find('p')
-
-summary = [tag.text]
-after = None
-
-while True:  # next paragraphs of summary until div tag
-    tag = tag.nextSibling
-    if isinstance(tag, bs4.element.Tag):
-        if tag.name == 'h2':
-            after = tag
-            break
-        elif tag.name == 'p':
-            summary.append(tag.text)
-
-tags = after.findNextSiblings(name='p')  # all paragraphs of text
-text = []
-for t in tags:
-    text.append(t.text)
-
-#print(summary)
-#print(text)
-
-"""CLEANING AND TOKENIZING DATA"""
-
-summary_combined = ''
-for paragraph in summary:
-    summary_combined = summary_combined + paragraph
-
-text_combined = ''
-for paragraph in text:
-    text_combined = text_combined + paragraph
-
-summary_cleaned = re.sub('\[[0-9]*\]', '', summary_combined)
-text_cleaned = re.sub('\[[0-9]*\]', '', text_combined)
-#print(summary_combined)
-#print(summary_cleaned)
-#print(text_combined)
-#print(text_cleaned)
-
-summary_tokens = nltk.word_tokenize(summary_cleaned)
-text_tokens = nltk.word_tokenize(text_cleaned)
-
-# exclude (,.
-
-exclude_list = [',', '.', '(', ')', '»', '«']
-processed_summary_tokens = [e.lower() for e in summary_tokens if e not in exclude_list]
-processed_text_tokens = [e.lower() for e in text_tokens if e not in exclude_list]
-all_tokens = processed_summary_tokens + processed_text_tokens
-
-print(processed_summary_tokens)
-print(processed_text_tokens)
-
-"""TEXT DATA"""
+    titles.append(title)
+    summaries.append(summary)
+    texts.append(text)
+    all_tokens = all_tokens + summary + text
 
 freq_distribution = nltk.FreqDist(all_tokens)
 #print(freq_distribution)
@@ -91,3 +100,4 @@ freq_distribution = nltk.FreqDist(all_tokens)
 
 nltk_text = nltk.Text(all_tokens)
 #print(nltk_text.collocations())
+
