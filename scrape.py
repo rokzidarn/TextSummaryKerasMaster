ffa3454bd0427e3a50b52eb58b70fca938d5dde6
Rok Zidarn <rz3817@student.uni-lj.si>
2018-12-07 18:59:06 +0100

    Bot works, need to exclude certain sections

--- scrape.py
+++ scrape.py
@@ -7,21 +7,38 @@ site = pywikibot.Site()
 category = pywikibot.Category(site, 'Category:Naravoslovje')
 
 generated = pagegenerators.CategorizedPageGenerator(category, recurse=2)
-pages = []
-num = 5
+urls = []
+titles = []
+articles = []
+summaries = []
+
+exclusion_sections = ["Glej tudi", "Viri", "Zunanje povezave", "Opombe", "Sklici"]
+
+limit = 5
 
 for page in generated:
+    if limit == 0:
+        break
+
     title = page.title()
-    text = page.text
-    text_length = len(text.split())
+    url = page.full_url()
 
-    if 1000 < text_length < 2000:
-        print(title)
-        pages.append(title)
-        page_py = wiki.page(title)
+    wiki_page = wiki.page(title)
+    article_text = wiki_page.text
+    article_summary = wiki_page.summary
+    article_length = len(article_text.split())
 
-        print(page_py.text[0:60])
+    print(".", end="", flush=True)
 
-        num = num - 1
-        if num == 0:
-            break
+    if 1000 < article_length < 2000:
+        limit = limit - 1
+
+        titles.append(title)
+        urls.append(url)
+        summaries.append(article_summary)
+        articles.append(article_text)
+
+        print("\n", title, url)
+        print(article_summary[0:100])
+        print(article_length)
+        #input("ENTER\n")
